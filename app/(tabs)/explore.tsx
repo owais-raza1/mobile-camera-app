c63ec0b70bb1141ca098bbaca29483a8c60f0652
owais-raza1 <syedowaisraza5@gmail.com>
2024-09-13 23:44:30 +0500

    first react native app

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,5 +1,5 @@
 import MapView, { Marker } from "react-native-maps";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { useState, useEffect } from "react";
 import * as Location from "expo-location";
 import tw from "twrnc";
@@ -7,6 +7,8 @@ import tw from "twrnc";
 export default function TabTwoScreen() {
   const [location, setLocation] = useState<any>(null);
   const [errorMsg, setErrorMsg] = useState<any>(null);
+  const [searchResult, setSearchResult] = useState<any>();
+  const [pickupLocation, setPickupLocation] = useState<any>();
 
   useEffect(() => {
     (async () => {
@@ -26,8 +28,50 @@ export default function TabTwoScreen() {
     })();
   }, []);
 
+  const findPickupLocation = (text: any) => {
+    const { latitude, longitude } = location.coords
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: "fsq3dDP+4pG7O7Qz/o77os/ht1kRQ9T/UrHbfeYt9gFT5mw=",
+      },
+    };
+
+    fetch(
+      `https://api.foursquare.com/v3/places/search?query=${text}&ll=${latitude},${longitude}&radius=100000`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setSearchResult(response.results))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <View style={tw`flex-1`}>
+      <View style={tw`mt-5 p-2`}>
+        <TextInput
+          placeholder="Search Pickup places"
+          style={tw`border`}
+          onChangeText={findPickupLocation}
+        />
+      </View>
+      {searchResult && !pickupLocation && (
+        <View style={tw`w-full`}>
+          {searchResult.map((item: any) => {
+            console.log(item.location);
+            
+            return (
+              <TouchableOpacity onPress={() => setPickupLocation(item)}>
+                <Text>
+                  {item.name} | {item.location.formatted_address}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      )}
       {location ? (
         <MapView
           region={{
